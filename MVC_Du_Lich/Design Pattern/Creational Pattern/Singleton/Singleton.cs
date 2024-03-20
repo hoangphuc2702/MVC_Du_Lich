f781f259c051a7dedd92b28f35625f898c61ba2f
hoangphuc27 <tonthathoangphuc270203@gmail.com>
2024-03-20 21:14:07 +0700

    singleton pattern

---
+++ MVC_Du_Lich/Design Pattern/Creational Pattern/Singleton/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using MVC_Du_Lich.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MVC_Du_Lich.Pattern.Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private List<TOUR> tours = new List<TOUR>();
+
+        //Private constructor is used to prevent
+        //creation of instances with 'new' keyword outside this class
+        private Singleton()
+        {
+        }
+
+        public void LazyInit(QLDULICHEntities db)
+        {
+            if(tours.Count == 0)
+            {
+                tours = db.TOURs.ToList();
+            }
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+
+        public List<TOUR> getTours
+        {
+            get { return tours; }
+        }
+
+        public void UpdateTour(QLDULICHEntities db)
+        {
+            tours.Clear();
+            LazyInit(db);
+        }
+    }
+}
