f744b3e27a8f3909a098fe8d65d8b47b8bf80f31
thaibinh77 <tranbinh772003@gmail.com>
2024-03-20 23:12:13 +0700

    builder pattern

---
+++ MVC_Du_Lich/Design Pattern/Creational Pattern/Builder/IBuilder.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Du_Lich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Du_Lich.Design_Pattern.Creational_Pattern.Builder
+{
+    public interface IBuilder
+    {
+        IBuilder ThemMaTour(string maTour);
+        IBuilder ThemTenTour(string tenTour);
+        IBuilder ThemGia(decimal gia);
+        IBuilder ThemSoLuong(int soLuong);
+        IBuilder ThemHinh1(string hinh1);
+        IBuilder ThemHinh2(string hinh2);
+        IBuilder ThemHinh3(string hinh3);
+        IBuilder ThemHinh4(string hinh4);
+        IBuilder ThemMoTa(string moTa);
+        IBuilder ThemSLConLai(int sLConLai);
+        IBuilder ThemNgayDiTour(DateTime ngayDiTour);
+        IBuilder ThemNgayKetThuc(DateTime ngayKetThuc);
+        IBuilder ThemMaDDi(int maDDi);
+        IBuilder ThemMaDDen(int maDDen);
+        IBuilder ThemMaLoaiTour(int maLoaiTour);
+        IBuilder ThemMaPT(int maPT);
+        IBuilder ThemMaLKS(int maLKS);
+        TOUR build();
+    }
+}
