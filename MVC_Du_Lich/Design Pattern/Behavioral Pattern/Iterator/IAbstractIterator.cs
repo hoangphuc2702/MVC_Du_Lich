2896c71a8b11bbe082bed78e5a93881523d65ddd
thaibinh77 <tranbinh772003@gmail.com>
2024-03-21 11:13:45 +0700

    iterator pattern

---
+++ MVC_Du_Lich/Design Pattern/Behavioral Pattern/Iterator/IAbstractIterator.cs
@@ -0,0 +1,16 @@
+﻿using MVC_Du_Lich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Du_Lich.Design_Pattern.Iterator
+{
+    interface IAbstractIterator
+    {
+        ThanhVien First();
+        ThanhVien Next();
+        bool hasNext { get; }
+        ThanhVien currentTV { get; }
+    }
+}
